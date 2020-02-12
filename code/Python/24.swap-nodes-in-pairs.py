37fc3135c925186aef661a54c693baea6a9abdc1
ctios <ctios@outlook.com>
2020-02-12 17:10:51 +0800

    24.swap-nodes-in-pairs

--- code/Python/24.swap-nodes-in-pairs.py
+++ code/Python/24.swap-nodes-in-pairs.py
@@ -4,22 +4,23 @@
 #         self.val = x
 #         self.next = None
 
+
 class Solution:
     def swapPairs(self, head: ListNode) -> ListNode:
         if not head or not head.next:
             return head
-        p=head
-        q=head.next
-        head=q
-        p.next=q.next
-        head.next=p
-        p=head.next.next
-        c=head.next
+        p = head
+        q = head.next
+        head = q
+        p.next = q.next
+        head.next = p
+        p = head.next.next
+        c = head.next
         while p and p.next:
-            q=p.next
-            p.next=q.next
-            c.next=q
-            q.next=p
-            c=p 
-            p=p.next
+            q = p.next
+            p.next = q.next
+            c.next = q
+            q.next = p
+            c = p
+            p = p.next
         return head
